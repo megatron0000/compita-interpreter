2e4e2267fb63b281e20f50ee0cbe215020a8848a
Vitor Arruda <vitor_pimenta97@hotmail.com>
2019-07-01 09:26:59 -0300

    Add more instruction/runtime definitions

--- ts/lab3456/intermediate/definitions.ts
+++ ts/lab3456/intermediate/definitions.ts
@@ -25,9 +25,9 @@ interface RelativeMemoryAddress {
   displacement: number
 }
 
-type MemoryAddress = AbsoluteMemoryAddress | RelativeMemoryAddress
+export type MemoryAddress = AbsoluteMemoryAddress | RelativeMemoryAddress
 
-type RegisterName = 'EIP' | 'ESP' | 'EBP' | 'ERV'
+type RegisterName = 'EIP' | 'ESP' | 'EBP' | 'ERV' | 'EHM' | 'R0' | 'R1' | 'R2'
 
 interface Register {
   kind: 'register'
@@ -49,28 +49,176 @@ export interface InstructionOperandKinds {
   'relative address': RelativeMemoryAddress
 }
 
-export type Instruction = MOV | ADD | JNE | NOP
+export type Instruction = InstructionKinds[keyof InstructionKinds]
 
 export interface InstructionKinds {
   'MOV': MOV
+  'ASS': ASS
+  'CAST': CAST
   'ADD': ADD
+  'SUB': SUB
+  'MULT': MULT
+  'DIV': DIV
+  'MOD': MOD
+  'NEG': NEG
+  'NOT': NOT
+  'OR': OR
+  'AND': AND
+  'JMP': JMP
   'JNE': JNE
+  'JEQ': JEQ
   'NOP': NOP
+  'PUSH': PUSH
+  'POP': POP
+  'CLT': CLT
+  'CLE': CLE
+  'CGT': CGT
+  'CGE': CGE
+  'CEQ': CEQ
+  'CNE': CNE
+  'READ': READ
+  'WRITE': WRITE
+  'CALL': CALL
+  'RET': RET
+  'HALT': HALT
 }
 
-export interface WritefulInstruction {
+
+/**
+ * Shorthand for "move". The destination's type is overwritten by
+ * the source's type
+ */
+export interface MOV {
+  kind: 'MOV'
+  source: InstructionOperand
   destination: Register | MemoryAddress
 }
 
-export interface MOV extends WritefulInstruction {
-  kind: 'MOV'
+/**
+ * Shorthand for "assign". The difference from "MOV" is: 
+ * The destination's type is *NOT* overwritten.
+ */
+export interface ASS {
+  kind: 'ASS'
   source: InstructionOperand
+  destination: Register | MemoryAddress
 }
 
-export interface ADD extends WritefulInstruction {
+/**
+ * Changes the type of `where` to `type`
+ */
+export interface CAST {
+  kind: 'CAST'
+  where: InstructionOperand
+  type: Exclude<VariableType, 'void'>
+}
+
+export interface ADD {
   kind: 'ADD'
   op1: InstructionOperand
   op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface SUB {
+  kind: 'SUB',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface MULT {
+  kind: 'MULT',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface DIV {
+  kind: 'DIV',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface MOD {
+  kind: 'MOD'
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface NEG {
+  kind: 'NEG'
+  op: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface NOT {
+  kind: 'NOT'
+  op: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface OR {
+  kind: 'OR',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface AND {
+  kind: 'AND',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface CLT {
+  kind: 'CLT',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface CLE {
+  kind: 'CLE',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface CGT {
+  kind: 'CGT',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface CGE {
+  kind: 'CGE',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface CEQ {
+  kind: 'CEQ',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface CNE {
+  kind: 'CNE',
+  op1: InstructionOperand
+  op2: InstructionOperand
+  destination: Register | MemoryAddress
+}
+
+export interface JMP {
+  kind: 'JMP'
+  jumpAddress: MemoryAddress
 }
 
 /**
@@ -79,12 +227,51 @@ export interface ADD extends WritefulInstruction {
 export interface JNE {
   kind: 'JNE'
   test: InstructionOperand
-  /**
-   * Where to jump to
-   */
-  address: MemoryAddress
+  jumpAddress: MemoryAddress
+}
+
+export interface JEQ {
+  kind: 'JEQ'
+  test: InstructionOperand
+  jumpAddress: MemoryAddress
 }
 
 export interface NOP {
   kind: 'NOP'
+}
+
+export interface PUSH {
+  kind: 'PUSH'
+  content: InstructionOperand
+}
+
+export interface POP {
+  kind: 'POP'
+  destination: Register
+}
+
+export interface READ {
+  kind: 'READ',
+  destination: Register | MemoryAddress
+}
+
+export interface WRITE {
+  kind: 'WRITE',
+  source: InstructionOperand
+}
+
+export interface CALL {
+  kind: 'CALL',
+  callAddress: MemoryAddress
+}
+
+export interface RET {
+  kind: 'RET'
+}
+
+/**
+ * Makes the program exit
+ */
+export interface HALT {
+  kind: 'HALT'
 }
--- ts/lab3456/runtime/definitions.ts
+++ ts/lab3456/runtime/definitions.ts
@@ -1,12 +1,29 @@
 /**
- * Registers:
+ * Special-purpose registers:
  *
- * ESP
- * EIP
- * EBP
- * ERV
+ * ESP: Stack pointer. 
+ *      Points at the element on top of the stack. 
+ *      Stack grows from high memory to low memory.
+ *      ESP begins at the highest memory address
+ * EIP: Instruction pointer
+ * EBP: Stack-frame-base pointer
+ * ERV: Return value holder
+ * EHM: Points to the highest memory address available
+ * 
+ * General-purpose registers:
+ * 
+ * R0: Always at 0
+ * R1
+ * R2
  *
- * No general-purpose registers
- *
+ */
+
+/**
+ * CALL x does not jump to x. Instead, it jumps to MEM[x]
+ */
+
+/**
+ * When an instruction begins execution, EIP is already pointing to the 
+ * next successive instruction
  */
 
